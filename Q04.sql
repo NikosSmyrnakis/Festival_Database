df5ce48027d8dafb0452ba100c531bdf9ec6143b
Marina <marina.frag3@gmail.com>
2025-05-07 14:05:08 +0300

    efwe
    dfs

--- Q04.sql
+++ Q04.sql
@@ -1,3 +1,4 @@
+
 SELECT 
     a.artist_name,
     ROUND(AVG(r.artist_performance), 2) AS avg_artist_performance,
@@ -7,5 +8,7 @@ JOIN ticket t ON r.ticket_ID = t.ticket_ID
 JOIN events e ON t.event_ID = e.event_ID
 JOIN performances p ON e.event_ID = p.event_ID AND t.event_ID = p.event_ID
 JOIN artist a ON p.artist_ID = a.artist_ID
-WHERE a.artist_ID = (SELECT artist_ID FROM artist WHERE artist_name = 'Joanna Smith')
+WHERE a.artist_ID = (SELECT artist_ID FROM artist WHERE artist_name = 'John Martin')
+    AND r.artist_performance IS NOT NULL
+    AND r.overall_impression IS NOT NULL
 GROUP BY a.artist_name;
