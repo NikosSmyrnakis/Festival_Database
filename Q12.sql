7ca879d6e72af54b2605d7985bd0f7e722b9a4a9
Marina <marina.frag3@gmail.com>
2025-05-08 11:03:10 +0300

    Update Q12.sql
    formated Q12

--- Q12.sql
+++ Q12.sql
@@ -1,10 +1,19 @@
-SELECT 
+SELECT
     f.festival_ID,
     e.festival_day,
     r.role,
     COUNT(DISTINCT r.personel_ID) AS required_personnel
-FROM role_of_personel_on_event r
-JOIN events e ON r.event_ID = e.event_ID
-JOIN festival f ON e.festival_ID = f.festival_ID
-GROUP BY f.festival_ID, e.festival_day, r.role
-ORDER BY f.festival_ID, e.festival_day, r.role;
+FROM
+    role_of_personel_on_event r
+    JOIN events e ON r.event_ID = e.event_ID
+    JOIN festival f ON e.festival_ID = f.festival_ID
+GROUP BY
+    f.festival_ID,
+    e.festival_day,
+    r.role
+ORDER BY
+    f.festival_ID,
+    e.festival_day,
+    r.role;
+
+-- ok
