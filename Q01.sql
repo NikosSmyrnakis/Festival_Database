7b0a88741ddb111163fc81910198dd186b09b980
Marina <marina.frag3@gmail.com>
2025-05-08 11:10:52 +0300

    wrr
    erwr

--- Q01.sql
+++ Q01.sql
@@ -1,9 +1,13 @@
 SELECT
-    YEAR(purchase_date) AS festival_year,
+    YEAR (purchase_date) AS festival_year,
     SUM(purchase_price) AS total_revenue
-FROM ticket
-WHERE purchase_price IS NOT NULL
-GROUP BY festival_year
-ORDER BY festival_year;
+FROM
+    ticket
+WHERE
+    purchase_price IS NOT NULL
+GROUP BY
+    festival_year
+ORDER BY
+    festival_year;
 
 -- ok
